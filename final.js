a65d1e27fccfd15dc2b509cf0a2269aa26b92411
carmen1810 <84483934+carmen1810@users.noreply.github.com>
2021-06-12 09:21:55 -0500

    Update final.js

--- final.js
+++ final.js
@@ -8,6 +8,13 @@ let promise = new Promise(
 
 
 //ejercicio2///
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  delay(3000).then(() => console.log('runs after 3 seconds'));
+
+//ejercicio 3///
 
     
     
